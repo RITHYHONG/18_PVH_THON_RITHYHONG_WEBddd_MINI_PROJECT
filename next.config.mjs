26737f7e38488528f106d1c77d3e55a39402555d
RITHYHONG <rithyhong8998@gmail.com>
2024-04-24 23:48:10 +0700

    Done
    Done

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: ['i.pinimg.com'],
+    },env: {
+        NEXT_APIURL: "http://110.74.194.123:8084",
+      }
+  };
+  
+  export default nextConfig;
+  
